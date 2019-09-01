92e8ab3e821c8dd45db9a145a529590d22496adf
Ali Shah <ali1195shah@gmail.com>
2019-09-01 01:09:22 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -35,8 +35,8 @@ def apply_clearance(cart)
   return cart
 end
 
-def checkout(array, coupons)
-  hash_cart = consolidate_cart(array)
+def checkout(cart, coupons)
+  hash_cart = consolidate_cart(cart)
   apply_coupons = apply_coupons(hash_cart, coupons)
   applied_discount = apply_clearance(apply_coupons)
   total = applied_discount.reduce(0) { |acc, (key, value)| acc += value[:price] + value[:count]}
